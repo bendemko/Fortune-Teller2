225308ea1d89e1aa50425013684df7da6de21f13
Ben Demko <bendemko7@gmail.com>
2016-07-08 21:02:34 -0400

    second commit

--- Fortune Teller 2/Program.cs
+++ Fortune Teller 2/Program.cs
@@ -10,6 +10,39 @@ namespace Fortune_Teller_2
     {
         static void Main(string[] args)
         {
+            //Read the user's first name from the user
+            Console.WriteLine("What is your first name?: ");
+            String firstName = Console.ReadLine();
+            Console.Clear();
+            //Read the user's last name from the user
+            Console.WriteLine("What is your last name?: ");
+            String lasttName = Console.ReadLine();
+            Console.Clear();
+            //Read the user's age from the user
+            Console.WriteLine("What is your age?: ");
+            string ageAsAString = Console.ReadLine();
+            int age = Convert.ToInt32(ageAsAString);
+            Console.Clear();
+            //Read the user's birth month from the user
+            Console.WriteLine("What is your birth month?: ");
+            String birthMonth = Console.ReadLine();
+            Console.Clear();
+            //Read the user's favorite color from the user
+            Console.WriteLine("What is your favorite ROYGBIV color?  If you don't know what a ROYGBIV color is, enter \"Help\": ");
+            String favoriteColor = Console.ReadLine();
+            //Read the number of siblings from the user
+            Console.WriteLine("How many siblings do you have?: ");
+            string numberOfSiblings = Console.ReadLine();
+            int siblings = Convert.ToInt32(numberOfSiblings);
+            Console.Clear();
+
+
+
+
+
+
+            Console.ReadKey();
+
         }
     }
 }
